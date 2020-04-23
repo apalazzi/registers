b488b909b9e60e9dabd2cd4042936e9dcb6c6b0f
Andrea Palazzi <palazziandrea@yahoo.it>
2020-04-23 21:32:29 +0200

    Rebase.

--- register.hpp
+++ register.hpp
@@ -1,7 +1,7 @@
 /*
  Copyright 2017 Andrea Palazzi <palazziandrea@yahoo.it>
 
- This file is part of halarm.
+ This file is part of mcu++.
 
  halarm is free software: you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -22,25 +22,37 @@
 #ifndef INCLUDE_REGISTER_HPP_
 #define INCLUDE_REGISTER_HPP_
 
+#include <climits>
 #include <cstddef>
 #include <cstdint>
+#include <functional>
 #include <stdexcept>
 
 namespace mcu {
 using addr_t=
     uint32_t;  // FIXME: this is architecture-dependent,
-               // move it somewhere else.
 using uint_t= unsigned int;
-template <typename T>
+
+template<typename T>
+constexpr int n_bits() {
+    return sizeof(T) * CHAR_BIT;
+}
+
+// template<typename T> constexpr max_N(const uint_t pos0,
+// const uint_t step) { 	return (n_bits<T>() - pos0) /
+// step;
+//}
+
+template<typename T>
 T mask(const uint_t pos, const uint_t len) {
     return (((static_cast<T>(1) << len) - 1) << (pos));
-};
+}
 
-template <typename T>
+template<typename T>
 void check_val_size(const T val, const uint_t len) {
     if (val >= (static_cast<T>(1) << len)) {
         throw(std::domain_error(
-            "Value exceeds Bitfield size."));
+            "Value exceeds storage size."));
     }
 }
 
@@ -70,55 +82,48 @@ constexpr bool can_set(RegisterType rt) {
 
 constexpr bool can_reset(RegisterType rt) {
     return (can_write(rt) ||
-            rt == RegisterType::read_clear_w1);
+            rt == RegisterType::read_clear_w0);
 }
 
-template <typename T, uint_t pos, uint_t len,
-          typename VAL= T,
-          RegisterType RT= RegisterType::read_write>
+template<typename T, uint_t pos, uint_t len,
+         typename VAL= T,
+         RegisterType RT= RegisterType::read_write>
 class Bit {
-   public:
+public:
     Bit()= delete;
     Bit(addr_t address)
-        :  // FIXME: use addr_t instead
-          raw(reinterpret_cast<T *>(address)) {
+        : raw(reinterpret_cast<T*>(address)) {
         check();
     }
-    Bit(T *const address): raw(address) { check(); }
+    Bit(T* const address): raw(address) { check(); }
     ~Bit()= default;
-    Bit &operator=(const VAL val) {
+    Bit& operator=(const VAL val) {
         set(val);
         return *this;
     }
-    bool operator==(VAL other) {
-        return get() ==
-               other;
-    }
-    bool operator!=(VAL other) {
-        return get() !=
-               other;
-    }
-    template <typename U= T,
-              typename= typename std::enable_if<len == 1,
-                                                U>::type>
+    bool operator==(VAL other) { return get() == other; }
+    bool operator!=(VAL other) { return get() != other; }
+    template<typename U= T,
+             typename=
+                 typename std::enable_if<len == 1, U>::type>
     operator bool() {
         return static_cast<bool>(get());
     }
-    template <typename U= T,
-              typename= typename std::enable_if<
-                  len == 1 && can_set(RT), U>::type>
+    template<typename U= T,
+             typename= typename std::enable_if<
+                 len == 1 && can_set(RT), U>::type>
     void set() {
         set(true);
     }
-    template <typename U= T,
-              typename= typename std::enable_if<
-                  len == 1 && can_reset(RT), U>::type>
+    template<typename U= T,
+             typename= typename std::enable_if<
+                 len == 1 && can_reset(RT), U>::type>
     void reset() {
         set(false);
     }
-    template <typename U= T,
-              typename= typename std::enable_if<
-                  len == 1 && can_write(RT), U>::type>
+    template<typename U= T,
+             typename= typename std::enable_if<
+                 len == 1 && can_write(RT), U>::type>
     void flip() {
         set(!get());
     }
@@ -138,8 +143,8 @@ class Bit {
             (*raw & mask<T>(pos, len)) >> pos);
     }
 
-   private:
-    volatile T *const raw;
+private:
+    volatile T* const raw;
     void check() const noexcept {
         static_assert(len > 0, "Bitfield has zero size.");
         static_assert(
@@ -148,42 +153,48 @@ class Bit {
     }
 };
 
-template <typename T, uint_t pos0, uint_t len,
-          uint_t step= len,
-          uint_t posN= (8 * sizeof(T) - pos0) / step,
-          RegisterType RT= RegisterType::read_write,
-          typename IDX= uint_t>
+template<typename base_t, uint_t pos0, uint_t len,
+         uint_t step= len,
+         uint_t N=
+             (n_bits<base_t>() - pos0 + (step - len)) /
+             step,
+         typename IDX= uint_t,
+         RegisterType RT= RegisterType::read_write>
 class BitArray {
     friend class reference;
 
-   public:
+public:
     BitArray()= delete;
     BitArray(const addr_t address)
-        : raw(reinterpret_cast<T *>(address)) {
+        : raw(reinterpret_cast<base_t*>(address)) {
         check();
     }
-    BitArray(addr_t *const address): raw(address) {
+    BitArray(addr_t* const address): raw(address) {
         check();
     }
-    void set(const uint_t idx, const T val,
+    void set(const IDX idx, const base_t val,
              const std::nothrow_t nothrow
              __attribute__((unused))) {
-        T m= mask<T>(idx * step + pos0, len);
-        *raw&= (~m);
-        *raw|= (val << (idx * step + pos0));
+        auto [m_idx, b_idx]= get_idx(idx);
+        base_t m= mask<base_t>(b_idx, len);
+        *(raw + m_idx)&= (~m);
+        *(raw + m_idx)|= (val << b_idx);
     }
 
-    void set(const uint_t idx, const T val) {
+    void set(const IDX idx, const base_t val) {
         check_index_overflow(idx);
         check_val_size(val, len);
         set(idx, val, std::nothrow);
     }
-    T get(const uint_t idx, const std::nothrow_t nothrow
-          __attribute__((unused))) const {
-        return (*raw & mask<T>(idx * step + pos0, len)) >>
-               (idx * step + pos0);
+    base_t get(const IDX idx, const std::nothrow_t nothrow
+               __attribute__((unused))) const {
+        auto [m_idx, b_idx]= get_idx(idx);
+
+        return (*(raw + m_idx) &
+                mask<base_t>(b_idx, len)) >>
+               b_idx;
     }
-    T get(const uint_t idx) const {
+    base_t get(const IDX idx) const {
         check_index_overflow(idx);
         return get(idx, std::nothrow);
     }
@@ -191,46 +202,53 @@ class BitArray {
     class reference {
         friend class BitArray;
 
-       public:
-        reference &operator=(const T val) {
+    public:
+        reference& operator=(const base_t val) {
             bf.set(idx, val);
             return *this;
         }
-        reference &operator=(const reference &other) {
+        reference& operator=(const reference& other) {
             // TODO: check if other is compatible with this
             bf.set(idx, other.bf.test(other.idx));
             return *this;
         }
-        operator T() const { return bf.get(idx); }
+        operator base_t() const { return bf.get(idx); }
         // T operator~() {}; // TODO: bitwise invert
-       private:
-        reference(const uint_t idx_, BitArray &bf_)
+    private:
+        reference(const IDX idx_, BitArray& bf_)
             : idx(idx_), bf(bf_) {}
-        const uint_t idx;
-        BitArray &bf;
+        const IDX idx;
+        BitArray& bf;
     };
-    T operator[](
-        const IDX idx) const {  // FIXME: constexpr?
-        return get(static_cast<uint_t>(idx));
+    base_t operator[](
+        const IDX idx) const {
+        return get(idx);
     }
     reference operator[](const IDX idx) {
-        return reference(static_cast<uint_t>(idx), *this);
+        return reference(idx, *this);
     }
-    template <typename U= T,
-              typename= typename std::enable_if<len == 1,
-                                                U>::type>
+    template<typename U= base_t,
+             typename=
+                 typename std::enable_if<len == 1, U>::type>
     operator bool() {
         return static_cast<bool>(get());
     }
 
-    // TODO: add operator bool() if len=1
-    // TODO: add some range check if IDX is int type to
-    // avoid accessing bits that should not be accessed
-   private:
-    volatile T *const raw;
+private:
+    volatile base_t* const raw;
+    std::tuple<const uint_t, const uint_t> get_idx(
+        const IDX idx) const {
+        const uint_t m_idx=
+            (static_cast<uint_t>(idx) * step + pos0) /
+            n_bits<base_t>();
+        const uint_t b_idx=
+            static_cast<uint_t>(idx) * step + pos0 -
+			n_bits<base_t>() * m_idx;
+        return {m_idx, b_idx};
+    }
     constexpr void check_index_overflow(
-        const uint_t idx) const {
-        if (pos0 + step * idx >= sizeof(T) * 8) {
+        const IDX idx) const {
+        if (static_cast<uint_t>(idx) >= N) {
             throw(std::out_of_range(
                 "BitfieldArray: idx exceeds type "
                 "capacity."));
@@ -242,9 +260,10 @@ class BitArray {
         static_assert(
             step >= len,
             "BitfieldArray step should be > len.");
-        static_assert(pos0 + len <= sizeof(T) * 8,
-                      "BitfieldArray pos0+len exceeds type "
-                      "capacity.");
+        static_assert(
+            pos0 + len <= sizeof(base_t) * CHAR_BIT,
+            "BitfieldArray pos0+len exceeds type "
+            "capacity.");
     }
 };
 
